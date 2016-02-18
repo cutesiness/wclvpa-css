b5793f433fcefeec5b22eb6f9d972301ff5cdd5d
Raisa Yang <raisa@alittle-town.net>
2016-02-18 15:23:40 -0500

    Make initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# WordCamp Lehigh Valley 2016 CSS
+This is the remote CSS file written for Lehigh Valley's WordCamp website for 2016. Built for the WordCamp base redux theme.
+
+
+## Team
+Front end development: Raisa Yang ([@sweetraiscake](http://twitter.com/sweetraiscake))
---
+++ gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    require: 'susy',
+                    style: 'compressed'
+                },
+                files: {
+                    'css/style.css' : 'scss/style.scss',
+                    'css/ie.css' : 'scss/ie.scss'
+                }
+            }
+        },
+        postcss: {
+            options: {
+                // or
+                map: {
+                    inline: false, // save all sourcemaps as separate files...
+                    annotation: 'css' // ...to the specified directory
+                },
+
+                processors: [
+                    // require('pixrem')(), // add fallbacks for rem units
+                    require('autoprefixer-core')({browsers: 'last 2 versions'}) // add vendor prefixes
+                    // require('cssnano')() // minify the result
+                ]
+            },
+            dist: {
+                src: 'css/style.css'
+            }
+        },
+        watch: {
+            css: {
+                files: ['scss/*.scss', 'scss/*/*.scss'],
+                tasks: ['sass', 'postcss']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('server',['connect']);
+    grunt.registerTask('default',['sass', 'postcss']);
+}
